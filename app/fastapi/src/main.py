39fc48e521cfae8161ca4d0170e17c2edc861f5c
Jordi Valls <jordi.vallspineda@axpo.com>
2024-09-03 09:35:57 +0200

    add flask app

---
+++ app/fastapi/src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+VERSION = "0.0.1"
+
+@app.get("/")
+async def root():
+    return {"message": f"Hello World from FastAPI! This is version {VERSION}"}
---
+++ app/flask/src/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = "0.0.1"
+
+@app.route("/")
+def hello_world():
+    return {"message": f"Hello World from Flask! This is version {VERSION}"}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
