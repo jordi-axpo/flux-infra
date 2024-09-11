ed03adf0026acdf79ac2f115df86b754d7ab9f33
Jordi Valls <jordi.vallspineda@axpo.com>
2024-09-11 16:58:28 +0200

    Add env variable

--- app/fastapi/src/main.py
+++ app/fastapi/src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from os import getenv
 
 app = FastAPI()
 
 VERSION = "0.0.8"
 
+ENV = getenv("ENV")
+
 @app.get("/")
 async def root():
-    return {"message": f"Hello World from FastAPI! This is version {VERSION}"}
+    return {"message": f"Hello World from FastAPI! This is version {VERSION} in environment {ENV}"}
