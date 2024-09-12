a2421a169efa1c9303d48d38164ff663d50a0126
Jordi Valls <jordi.vallspineda@axpo.com>
2024-09-12 09:36:44 +0200

    Add Flask implementation

--- app/flask/src/main.py
+++ app/flask/src/main.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from os import getenv
 
 app = Flask(__name__)
 
 VERSION = "0.0.2"
 
+ENV = getenv("ENV")
+
 @app.route("/")
 def hello_world():
-    return {"message": f"Hello World from Flask! This is version {VERSION}"}
+    return {"message": f"Hello World from Flask! This is version {VERSION} in environment {ENV}"}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
